fd1b560ff83214b166a19881fbb288b0eacd4b65
Prema9738 <premapremu9738@gmail.com>
2026-08-24 14:56:17 +0530

    Update and rename app_py (2) (1).py to app_py.py

--- app_py.py
+++ app_py.py
@@ -26,7 +26,7 @@ st.write(
     "engagement, burnout risk, workload stress and career-stage patterns."
 )
 
-df = pd.read_csv("/content/Palo Alto Networks.csv")
+df = pd.read_csv("Palo Alto Networks.csv")
 
 df.head()
 
@@ -329,7 +329,3 @@ df.to_csv(
     "employee_engagement_analysis.csv",
     index=False
 )
-
-from google.colab import files
-
-files.download("employee_engagement_analysis.csv")
